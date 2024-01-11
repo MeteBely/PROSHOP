9daa9054c3d34f76881a7b89b6f196685239becf
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-12 00:26:23 +0300

    router tanımlanması

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
