f7d7da9a0072842a4bfa6ce68e2ca011b0664d53
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-12 00:26:32 +0300

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // proxy requests prefixed '/api' and '/uploads'
+  server: {
+    // proxy requests prefixed '/api' and '/uploads'
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/uploads': 'http://localhost:5000',
+    },
+  },
+});
