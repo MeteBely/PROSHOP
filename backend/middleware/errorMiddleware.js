3846df243590aa21276af8850ac70b470486c9c8
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-15 23:49:39 +0300

    middlewares

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,22 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode; //error'a gelirse status'ü 200'den 500'e çevireceğiz.
+  let message = err.message;
+
+  //Check for mongoose bad objectId
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    message = 'Resource not found';
+    statusCode = 404;
+  }
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? 'hi' : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
