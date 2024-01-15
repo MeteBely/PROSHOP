e8cb8481de5cbd491a45860d9d786827d4088ff6
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-15 23:49:54 +0300

    redux ve route geçilmesi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,18 +5,24 @@ import './index.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import ProductPage from './pages/ProductPage.jsx';
+import { Provider } from 'react-redux';
+import store from './store.jsx';
+import CartPage from './pages/CartPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
-      <Route index={true} path="/product/:id" element={<ProductPage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
