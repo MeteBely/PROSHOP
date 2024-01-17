1ed6914084103f35f8fc077a0ced2789b96aaab2
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-17 21:11:57 +0300

    token döndüren utility

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+  //SET HWT as http-only cookie
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000, //ms = 30 days
+  });
+};
+
+export default generateToken;
