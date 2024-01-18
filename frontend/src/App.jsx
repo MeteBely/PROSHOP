292555be09f0184bbd60a9c2ff1a6588739330bc
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-19 01:44:34 +0300

    react toastify importu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      <ToastContainer />
     </>
   );
 }
