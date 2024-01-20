599eddf8103db7a0a1a99e819d3a784d7856c5da
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-20 19:09:16 +0300

    order routesinin eklenmesi

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
