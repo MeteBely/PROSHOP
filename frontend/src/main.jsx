620d1922f5598c2c99f26ff31fd786591e399fc7
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-20 19:08:30 +0300

    private routeler eklenmesi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,12 @@ import { Provider } from 'react-redux';
 import store from './store.jsx';
 import CartPage from './pages/CartPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+import ShippingPage from './pages/ShippingPage.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import PaymentPage from './pages/PaymentPage.jsx';
+import PlaceOrderPage from './pages/PlaceOrderPage.jsx';
+import OrderPage from './pages/OrderPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +23,13 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/placeorder" element={<PlaceOrderPage />} />
+        <Route path="/order/:id" element={<OrderPage />} />
+      </Route>
     </Route>
   )
 );
