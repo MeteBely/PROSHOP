47c7bd916bd2b0cbbf6d16f1789c2645ecb80390
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-20 19:09:41 +0300

    header'e giriş yapmış kullanıcıya yönelik işlemler

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,28 @@
 import { FaShoppingCart } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
 import { TbSquareRoundedLetterP } from 'react-icons/tb';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { useLogoutMutation } from '../slices/usersApiSlice.jsx';
+import { logout } from '../slices/authSlice.jsx';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
   const [openMenu, setOpenMenu] = useState(false);
-  const logoutHandler = () => {};
+  const [logoutApiCall] = useLogoutMutation();
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <header className="">
       <nav className="">
@@ -20,33 +33,39 @@ const Header = () => {
               ProShop
             </Link>
           </div>
-          <div className="flex flex-row gap-8 relative">
-            <Link to="/cart" className="flex flex-row items-center gap-[4px]">
-              <FaShoppingCart />
-              Cart
-            </Link>
-            {cartItems.length > 0 && <div className="absolute left-[35%] bg-[#00B394] rounded-[50%] w-[25px] text-center text-white font-semibold">{cartItems.reduce((a, c) => a + c.qty, 0)}</div>}
-            <Link to="/login" className="flex flex-row items-center gap-[4px]">
-              <FaUser />
-              Sign In
-            </Link>
-            {userInfo ? (
-              <>
-                <div className="relative">
-                  <button onClick={() => setOpenMenu(!openMenu)} className="w-20 h-8 rounded-full">
-                    {userInfo.name}
-                  </button>
-                  {openMenu && (
-                    <div className="absolute right-0 mt-3 w-[200px] bg-white shadow-lg shadow-gray-300 flex flex-col items-start">
-                      <Link to="/profile">Profile</Link>
-                      <button onClick={() => logoutHandler()}>Logout</button>
-                    </div>
-                  )}
-                </div>
-              </>
-            ) : (
-              ''
-            )}
+          <div className="flex flex-row gap-8 justify-center items-center">
+            <div className="relative">
+              <Link to="/cart" className="flex flex-row items-center gap-[4px]">
+                <FaShoppingCart />
+                Cart
+              </Link>
+              {cartItems.length > 0 && <div className="absolute top-0 left-[52px] bg-[#00B394] rounded-[50%] w-[25px] text-center text-white font-semibold">{cartItems.reduce((a, c) => a + c.qty, 0)}</div>}
+            </div>
+            <div>
+              <Link to="/login" className="flex flex-row items-center gap-[4px]">
+                <FaUser />
+                Sign In
+              </Link>
+            </div>
+            <div>
+              {userInfo ? (
+                <>
+                  <div className="relative">
+                    <button onClick={() => setOpenMenu(!openMenu)} className="w-[100px] h-8 rounded-full">
+                      {userInfo.name}
+                    </button>
+                    {openMenu && (
+                      <div className="absolute right-0 mt-3 w-[200px] bg-white shadow-lg shadow-gray-300 flex flex-col items-start">
+                        <Link to="/profile">Profile</Link>
+                        <button onClick={() => logoutHandler()}>Logout</button>
+                      </div>
+                    )}
+                  </div>
+                </>
+              ) : (
+                ''
+              )}
+            </div>
           </div>
         </div>
       </nav>
