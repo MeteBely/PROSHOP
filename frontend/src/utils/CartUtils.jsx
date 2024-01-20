0ecfcfa8cb0d1c918bc9c2d67bfaea6aa17d0f9a
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-20 19:09:03 +0300

    hata giderilmesi

--- frontend/src/utils/CartUtils.jsx
+++ frontend/src/utils/CartUtils.jsx
@@ -1,16 +1,22 @@
 export const addDecimals = (num) => {
-  return (Math.round(num / 100) * 100).toFixed(2);
+  return (Math.round(num * 100) / 100).toFixed(2);
 };
 
 export const updateCart = (state) => {
-  //calculate items price
-  state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
-  //calculate shipping price 100 dolardan fazlaysa bedava, azsa 10 dolar
-  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-  //calculate tax price(15%)
-  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-  //calculate total price
-  state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2);
+  const itemsPrice = state.cartItems.reduce((acc, item) => acc + (item.price * 100 * item.qty) / 100, 0);
+  state.itemsPrice = addDecimals(itemsPrice);
+
+  // Calculate the shipping price
+  const shippingPrice = itemsPrice > 100 ? 0 : 10;
+  state.shippingPrice = addDecimals(shippingPrice);
+
+  // Calculate the tax price
+  const taxPrice = 0.15 * itemsPrice;
+  state.taxPrice = addDecimals(taxPrice);
+
+  const totalPrice = itemsPrice + shippingPrice + taxPrice;
+  // Calculate the total price
+  state.totalPrice = addDecimals(totalPrice);
 
   localStorage.setItem('cart', JSON.stringify(state));
 
