736a43fff061454cd84ffb44ea0b1d1adfe5c45a
MeteBely <metehanmuradoglu17@gmail.com>
2024-01-31 23:35:21 +0300

    routes işlemleri

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getProductById, getProducts, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { getProductById, getProducts, createProduct, updateProduct, deleteProduct, createProductReview, getTopProducts } from '../controllers/productController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/top').get(getTopProducts);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
